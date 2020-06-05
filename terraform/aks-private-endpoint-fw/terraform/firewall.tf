b217a5271e436b04884cd916d605f6c804bb53a8
chris bellee <cbellee@users.noreply.github.com>
2020-06-05 11:07:27 +1000

    added firewall logging

--- terraform/aks-private-endpoint-fw/terraform/firewall.tf
+++ terraform/aks-private-endpoint-fw/terraform/firewall.tf
@@ -1,6 +1,7 @@
 locals {
-  fw_pip_name = "${var.prefix}-fw-pip"
-  fw_name     = "${var.prefix}-fw"
+  fw_pip_name         = "${var.prefix}-fw-pip"
+  fw_name             = "${var.prefix}-fw"
+  fw_diagnostics_name = "${var.prefix}-fw-diagnostics"
 }
 
 resource azurerm_public_ip "az_firewall_pip" {
@@ -24,4 +25,39 @@ resource azurerm_firewall "az_firewall" {
   }
 
   tags = var.tags
+}
+
+resource "azurerm_monitor_diagnostic_setting" "az_firewall_diagnostics" {
+  name                       = local.fw_diagnostics_name
+  target_resource_id         = azurerm_firewall.az_firewall.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.workspace.id
+
+  log {
+    category = "AzureFirewallApplicationRule"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = 7
+    }
+  }
+
+  log {
+    category = "AzureFirewallNetworkRule"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = 7
+    }
+  }
+
+  metric {
+    category = "AllMetrics"
+
+    retention_policy {
+      enabled = true
+      days    = 7
+    }
+  }
 }
