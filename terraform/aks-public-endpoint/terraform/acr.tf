bae249d1bac08bcec57dda2b77f01c950550428e
chris bellee <cbellee@users.noreply.github.com>
2020-07-09 10:32:41 +1000

    added 'acrpull' role to aks user managed identity

--- terraform/aks-public-endpoint/terraform/acr.tf
+++ terraform/aks-public-endpoint/terraform/acr.tf
@@ -13,5 +13,5 @@ resource "azurerm_container_registry" "acr" {
 resource "azurerm_role_assignment" "aks_acrpull_role_assignment" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity.client_id
+  principal_id         = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity.0.object_id
 }
