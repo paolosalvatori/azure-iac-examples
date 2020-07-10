5db97467709701160d481651ce238b8dccec9478
chris bellee <cbellee@users.noreply.github.com>
2020-07-10 15:47:50 +1000

    added unique name for la workspace

--- terraform/aks-availability-zones/terraform/log_analytics.tf
+++ terraform/aks-availability-zones/terraform/log_analytics.tf
@@ -1,9 +1,13 @@
 locals {
-  ws_name = "${var.prefix}-ws"
+  ws_name = "${var.prefix}"
+}
+
+resource "random_id" "log_analytics_workspace_unique_name" {
+  byte_length = 8
 }
 
 resource azurerm_log_analytics_workspace "workspace" {
-  name                = local.ws_name
+  name                = "${local.ws_name}-${lower(random_id.log_analytics_workspace_unique_name.hex)}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "PerGB2018"
