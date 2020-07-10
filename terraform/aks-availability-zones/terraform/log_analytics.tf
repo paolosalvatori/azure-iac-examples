18b9e9547dee9bf146a65bedeb5d732b70618f18
chris bellee <cbellee@users.noreply.github.com>
2020-07-10 14:56:49 +1000

    removed uuid from log analytics workspace name

--- terraform/aks-availability-zones/terraform/log_analytics.tf
+++ terraform/aks-availability-zones/terraform/log_analytics.tf
@@ -1,11 +1,11 @@
 locals {
-  ws_name = "${var.prefix}"
+  ws_name = "${var.prefix}-ws"
 }
 
 resource "random_uuid" "az_monitor_uuid" { }
 
 resource azurerm_log_analytics_workspace "workspace" {
-  name                = "${local.ws_name}-${random_uuid.az_monitor_uuid.az_monitor_uuid}-ws"
+  name                = local.ws_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "PerGB2018"
