b04f8cee0db3d7d8db67db25a4d1a235f7de11b2
chris bellee <cbellee@users.noreply.github.com>
2020-07-10 14:08:26 +1000

    removed addon profile

--- terraform/aks-availability-zones/terraform/aks.tf
+++ terraform/aks-availability-zones/terraform/aks.tf
@@ -23,12 +23,13 @@ resource azurerm_kubernetes_cluster "aks_cluster" {
   dns_prefix              = local.aks_cluster_name
   kubernetes_version      = var.kubernetes_version
 
-  addon_profile {
-   /*  oms_agent {
+  /* addon_profile {
+     oms_agent {
       enabled                    = true
       log_analytics_workspace_id = azurerm_log_analytics_workspace.workspace.id
-    } */
+    }
   }
+  */
 
   linux_profile {
     admin_username = "localadmin"
