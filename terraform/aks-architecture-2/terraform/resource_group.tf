2747416613d93ae5c4d5b61601e78748fe3a77d4
chris bellee <cbellee@users.noreply.github.com>
2020-08-18 09:55:15 +1000

    fixed for_each statement

--- terraform/aks-architecture-2/terraform/resource_group.tf
+++ terraform/aks-architecture-2/terraform/resource_group.tf
@@ -1,5 +1,5 @@
 resource azurerm_resource_group "rg" {
-    for_each = var.resource_groups
+    for_each = toset(var.resource_groups)
 
     name = "${var.prefix}-${each.value.name}"
     location = var.location
