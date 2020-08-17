069189a2b8880d2f2896753efc6678bf6b85610e
chris bellee <cbellee@users.noreply.github.com>
2020-08-18 08:39:38 +1000

    fixed for_each statement

--- terraform/aks-architecture-2/terraform/main.tf
+++ terraform/aks-architecture-2/terraform/main.tf
@@ -4,7 +4,7 @@ terraform {
     resource_group_name  = "terraform-rg"
     storage_account_name = "stortf38f883"
     container_name       = "terraform-state"
-    key                  = "terraform-aks-architecture-1.tfstate"
+    key                  = "terraform-aks-architecture-2.tfstate"
   }
 }
 
