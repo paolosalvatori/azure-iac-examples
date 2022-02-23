a465875f2d25f06851dca5833d89c8c087bbe114
Chris Bellee <8634922+cbellee@users.noreply.github.com>
2022-02-24 10:35:47 +1100

    tidied up example

--- bicep/app-svc-python/app.py
+++ bicep/app-svc-python/app.py
@@ -8,72 +8,38 @@ def home():
     return jsonify({"response": "OK", "code": 200})
 
 
-@app.route('/seed', methods=['GET'])
-def db_create():
+@app.route('/info', methods=['GET'])
+def get_dbhost():
     import psycopg2
     import os
+    import socket
 
-    # get env vars
-    DB_CXN_STR = os.getenv('DB_CONNECTION_STRING')
-    print("DB Connection String: ", DB_CXN_STR)
+    response = "no serverdata found..."
 
-    conn = psycopg2.connect(DB_CXN_STR)
-    print("Connection established")
+    try:
+        # get env var
+        DB_CXN_STR = os.getenv('DB_CONNECTION_STRING')
 
-    cursor = conn.cursor()
+        # connect to db server
+        conn = psycopg2.connect(DB_CXN_STR)
+        cursor = conn.cursor()
 
-    # Drop previous table of same name if one exists
-    cursor.execute("DROP TABLE IF EXISTS inventory;")
-    print("Finished dropping table (if existed)")
+        # get database server ip address
+        cursor.execute("SELECT inet_server_addr FROM inet_server_addr();")
+        row = cursor.fetchall()
+        serverIp = str(row[0])
 
-    # Create a table
-    cursor.execute(
-        "CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);")
-    print("Finished creating table")
-
-    # Insert some data into the table
-    cursor.execute(
-        "INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("apple", 120))
-    cursor.execute(
-        "INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("banana", 340))
-    cursor.execute(
-        "INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("pear", 90))
-
-    # Clean up
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-    return 'complete'
-
-
-@app.route('/inventory', methods=['GET'])
-def db_read():
-    import psycopg2
-    import os
-
-    # get env vars
-    DB_CXN_STR = os.getenv('DB_CONNECTION_STRING')
-
-    conn = psycopg2.connect(DB_CXN_STR)
-    print("Connection established")
-
-    cursor = conn.cursor()
-    cursor.execute("SELECT name, quantity FROM inventory")
-    rows = cursor.fetchall()
-
-    results = [
-        {
-            "name": str(row[0]),
-            "quantity": str(row[1])
-        } for row in rows]
-
-    # Clean up
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-    return jsonify(results)
+        response =  "Connected to Database Server IP: " + serverIp
+        return response
+    except Exception as err:
+        return("error: (" + err  + ") connecting to database with connection string: " + DB_CXN_STR)
+    finally:
+        # dispose
+        conn.commit()
+        cursor.close()
+        conn.close()
+        
+        return response
 
 if __name__ == '__main__':
     app.run(debug=True)
