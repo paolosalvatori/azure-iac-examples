9ccbb86668607eb5da0bcf4847fb3a9ed545a21a
Chris Bellée <8634922+cbellee@users.noreply.github.com>
2022-07-28 22:23:49 +1000

    dd

--- bicep/ag-apim-aks/src/spa/src/authConfig.js
+++ bicep/ag-apim-aks/src/spa/src/authConfig.js
@@ -53,7 +53,6 @@ export const msalConfig = {
  */
 export const loginRequest = {
     scopes: [`${msalConfig.auth.clientId}/.default`]
-   // scopes: ["api://ddbcb46c-7741-46d3-b0cf-7b4e5ba5464d/Product.Read"]
 };
 
 export const orderApi = {
