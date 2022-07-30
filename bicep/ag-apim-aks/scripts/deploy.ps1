98ee03bee686c94477bbb242028030b6054bcc1d
Chris Bellée <8634922+cbellee@users.noreply.github.com>
2022-07-30 23:06:06 +1000

    corrected typo

--- bicep/ag-apim-aks/scripts/deploy.ps1
+++ bicep/ag-apim-aks/scripts/deploy.ps1
@@ -393,5 +393,5 @@ $(Get-Content -Path ../manifests/order.yaml) -replace "{{IMAGE_TAG}}", "$($deplo
 $(Get-Content -Path ../manifests/product.yaml) -replace "{{IMAGE_TAG}}", "$($deployment.Outputs.acrName.Value).azurecr.io/$productApiImageName" -replace "{{SVC_IP_ADDRESS}}", $oproductApiSvcIp | kubectl apply -f -
 $(Get-Content -Path ../manifests/spa.yaml) -replace "{{IMAGE_TAG}}", "$($deployment.Outputs.acrName.Value).azurecr.io/$spaImageName" -replace "{{SVC_IP_ADDRESS}}", $reactSpaSvcIp | kubectl apply -f -
 
-# add admin consent for rct spa app registration
+# add admin consent for react spa app registration
 az ad app permission admin-consent --id $appRegistrations."$identityPrefix-react-spa".AppId
